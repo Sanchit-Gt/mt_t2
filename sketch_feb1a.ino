c69a147d03a9107d854cc6c87e03e52f158afef9
Sanchit-Gt <127891513+Sanchit-Gt@users.noreply.github.com>
2024-02-01 13:02:41 +0530

    Add files via upload

---
+++ sketch_feb1a.ino
@@ -0,0 +1,34 @@
+const int pirPin = 2;   
+const int ledPin = 13;  
+
+volatile int pirState = LOW;
+int val = 0;            
+
+void setup() {
+  pinMode(ledPin, OUTPUT); 
+  pinMode(pirPin, INPUT);  
+  Serial.begin(9600);      
+
+  attachInterrupt(digitalPinToInterrupt(pirPin), pirInterrupt, CHANGE);
+}
+
+void loop() {
+}
+
+void pirInterrupt() {
+  val = digitalRead(pirPin); 
+
+  if (val == HIGH) {  
+    digitalWrite(ledPin, HIGH); 
+    if (pirState == LOW) {
+      Serial.println("Motion detected!");
+      pirState = HIGH;
+    }
+  } else {
+    digitalWrite(ledPin, LOW);
+    if (pirState == HIGH){
+      Serial.println("Motion stopped!");
+      pirState = LOW;
+    }
+  }
+}
